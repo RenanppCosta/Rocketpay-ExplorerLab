4353bdac451f1c28bf374c136ddfdf79c535e9a4
Renanppcosta <renanppcosta@gmail.com>
2022-10-19 02:19:38 -0300

    Progresso Aula 2 Finalizado

--- src/main.js
+++ src/main.js
@@ -1,1 +1,88 @@
 import "./css/index.css"
+import IMask from "imask";
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const logo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setTypeCard(type) {
+    const colors = {
+        visa: ["#436D99","#C69347"],
+        mastercard:["#DF6F29","#C3129C"],
+        nubank:["#06315E","#55034D"],
+        default:["black","gray"],
+        bradesco:["#5A0101","#B50808"]
+    }
+
+    ccBgColor01.setAttribute("fill", colors[type][0]);
+    ccBgColor02.setAttribute("fill", colors[type][1]);
+    logo.setAttribute("src", `cc-${type}.svg`);
+
+}
+
+globalThis.setTypeCard = setTypeCard;
+setTypeCard("bradesco");
+
+//security code
+const securityCode = document.getElementById("security-code");
+const securityCodePattern = {
+    mask: "0000"
+}
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+
+const expirationDate = document.getElementById("expiration-date");
+const expirationDatePattern = {
+    mask: "MM{/}YY",
+    blocks: {
+        MM:{
+            mask: IMask.MaskedRange,
+            from: 1,
+            to: 12,
+         },
+        YY:{
+            mask: IMask.MaskedRange,
+            from: String(new Date().getFullYear()).slice(2),
+            to: String(new Date().getFullYear() + 10).slice(2),
+        }
+    }
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern)
+
+const cardNumber = document.getElementById("card-number");
+const cardNumberPattern = {
+    mask: [
+        {
+            mask:"0000 0000 0000 0000",
+            regex: /^4\d{0,15}/,
+            cardtype: "visa"
+        },
+        {
+            mask:"0000 0000 0000 0000",
+            regex: /(^5[1-5]\d{0,2}|^22[2-9]\d|^2[3-7]\d{0,2})\d{0,12}/,
+            cardtype: "mastercard"
+        },
+        {
+            mask:"0000 0000 0000 0000",
+            regex: "",
+            cardtype: "nubank"
+        },
+        {
+            mask:"0000 0000 0000 0000",
+            cardtype: "default"
+        },
+        {
+            mask:"0000 0000 0000 0000",
+            regex: "",
+            cardtype: "bradesco"
+        }
+    ],
+    dispatch: function(appendend, dynamicMasked){
+        const number = (dynamicMasked.value + appendend).replace(/\D/g,"");
+        const foundMask = dynamicMasked.compiledMasks.find(({regex})=> number.match(regex));
+
+        return foundMask;
+    }
+}
+
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
